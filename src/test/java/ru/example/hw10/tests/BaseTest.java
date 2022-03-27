7b3086dcf3567b4aa140bf0fbd1caa9d541cf877
Andrey Panin <avpanin@alfabank.ru>
2022-03-27 09:08:51 +0300

    hw10: добавил запуск через selenoid и attach файлы

---
+++ src/test/java/ru/example/hw10/tests/BaseTest.java
@@ -0,0 +1,36 @@
+package ru.example.hw10.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.example.hw10.helper.Attach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/ru/example/hw10/tests/StudentRegistrationFormTest.java
@@ -0,0 +1,66 @@
+package ru.example.hw10.tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import ru.example.hw10.pages.PageFormStudents;
+import ru.example.hw10.testData.Student;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentRegistrationFormTest extends BaseTest {
+    final String PATH_TO_FILE = "src/test/resources/english.jpg";
+
+    @Test
+    public void inputValidData() {
+        Faker faker = new Faker();
+        Student student = Student.newBuilder()
+                .firstName(faker.name().firstName())
+                .lastName(faker.name().lastName())
+                .userEmail(faker.internet().emailAddress())
+                .userNumber("8904117400")
+                .gender("Male")
+                .dateOfBirth("01 May 1993")
+                .subjects("Science")
+                .hobbies("Reading")
+                .address(faker.address().streetAddress())
+                .state("Haryana")
+                .city("Panipat").build();
+
+        open("/automation-practice-form", PageFormStudents.class)
+                .setFirstName(student.getFirstName())
+                .setLastName(student.getLastName())
+                .setUserEmail(student.getUserEmail())
+                .setUserNumber(student.getUserNumber())
+                .setGender(student.getGender())
+                .setBirthDay(student.getDateOfBirth())
+                .setSubject(student.getSubjects())
+                .setHobbies(student.getHobbies())
+                .setAddress(student.getAddress())
+                .setStateAndCity(student.getState(), student.getCity())
+                .clickSubmit()
+                .assertSuccessfulText("Thanks for submitting the form")
+                .assertSuccessfulList(student.getExpRes());
+    }
+
+    @Test
+    public void inputRequiredValidData() {
+        Faker faker = new Faker();
+        Student student = Student.newBuilder()
+                .firstName(faker.name().firstName())
+                .lastName(faker.name().lastName())
+                .userNumber("8904117400")
+                .gender("Female")
+                .dateOfBirth("30 May 1993")
+                .build();
+
+        open("/automation-practice-form", PageFormStudents.class)
+                .setFirstName(student.getFirstName())
+                .setLastName(student.getLastName())
+                .setUserNumber(student.getUserNumber())
+                .setGender(student.getGender())
+                .setBirthDay(student.getDateOfBirth())
+                .clickSubmit()
+                .assertSuccessfulText("Thanks for submitting the form")
+                .assertSuccessfulList(student.getExpRes());
+    }
+}
